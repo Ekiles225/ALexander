4fae810f493c9a54af280694ac01b16861fa7856
Ekiles225 <ekiles07@outlook.com>
2024-05-28 20:58:56 -0800

    JavaScript

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,23 +1,23 @@
-// function setResultado(value){
-//     document.getElementById('resultado').innerHTML = value;
-// }
+function setResultado(value){
+    document.getElementById('resultado').innerHTML = value;
+}
 
-// function getResultado(){
-//     return (document.getElementById('resultado').innerHTML);
-// }
+function getResultado(){
+    return (document.getElementById('resultado').innerHTML);
+}
 
-// function add(key){
-//     var resultado = getResultado();
-//     if(resultado != '0' || isNaN(key)) setResultado(resultado + key);
-//     else setResultado(key);
-// }
+function add(key){
+    var resultado = getResultado();
+    if(resultado != '0' || isNaN(key)) setResultado(resultado + key);
+    else setResultado(key);
+}
 
-// function calculo(){
-//     var result = eval(getResultado()); 
-//     setResultado(result);
-// }
+function calculo(){
+    var result = eval(getResultado()); 
+    setResultado(result);
+}
 
-// function del(){
-//     // Pone a cero el contenido de la pantalla de la calculadora.
-//     setResultado(0);
-// }
+function del(){
+    // Pone a cero el contenido de la pantalla de la calculadora.
+    setResultado(0);
+}
