603a74254df251ec6fd55d0536f67294199f33c0
Ekiles225 <ekiles07@outlook.com>
2024-05-28 20:58:17 -0800

    HTML

---
+++ js/myJavaScript.js
@@ -0,0 +1,23 @@
+// function setResultado(value){
+//     document.getElementById('resultado').innerHTML = value;
+// }
+
+// function getResultado(){
+//     return (document.getElementById('resultado').innerHTML);
+// }
+
+// function add(key){
+//     var resultado = getResultado();
+//     if(resultado != '0' || isNaN(key)) setResultado(resultado + key);
+//     else setResultado(key);
+// }
+
+// function calculo(){
+//     var result = eval(getResultado()); 
+//     setResultado(result);
+// }
+
+// function del(){
+//     // Pone a cero el contenido de la pantalla de la calculadora.
+//     setResultado(0);
+// }
